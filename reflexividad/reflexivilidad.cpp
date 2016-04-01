e4046a8999e56b0cfcc22336800e2506c481b879
luchoman <luchoman08@gmail.com>
2016-04-01 08:29:30 -0500

    transitividad y reflexibilidad probadas

--- reflexividad/reflexivilidad.cpp
+++ reflexividad/reflexivilidad.cpp
@@ -6,8 +6,9 @@
 using namespace std;
 
 typedef map<string, map<string,int>*> adyacencia; //<elemento, vertices>
-typedef pair<string, map<string,int>*> ii;
-typedef pair<string, int> iii;
+typedef pair<string, map<string,int>*> im;
+typedef pair<string, int> si;
+FILE * pFile;
 adyacencia  ady;
 vector<pair<string, string>> relaciones;
 vector<string> elementos;
@@ -25,26 +26,15 @@ int adicionar_tupla(string a, string b)
 	if(p == ady.end())
 	{
 		map<string,int>* s = new map<string,int> ;
-		ady.insert(ii(a, s));
+		s->insert(si(b, 0));
+		ady.insert(im(a, s));
 		return 0;
 	}
 	else
 	{
-		p->second->insert(iii(b,0));
+		p->second->insert(si(b,0));
 		return 1;
 	}
-}
-void ingresar_elementos()
-{
-	char tmp_c[256];
-	string tmp;
-	scanf("%d",&n_elementos);
-	for(int i =0; i<n_elementos;i++)
-	{
-	scanf("%s", tmp_c);
-	tmp = tmp_c;
-	elementos.push_back(tmp_c);
-    }
 }
 bool adyaccencia_a_b(string a, string b)
 {
@@ -53,6 +43,7 @@ bool adyaccencia_a_b(string a, string b)
   return false;
   else
   {
+	  
 	 map<string,int>::iterator q = p->second->find(b);
 	 if(q==p->second->end())
 	 return false;
@@ -62,20 +53,37 @@ bool adyaccencia_a_b(string a, string b)
 }
 bool reflexividad()
 {
-	for(int i = 0; i < elementos.size(); i++)
+	int size_elementos = elementos.size();
+	for(int i = 0; i < size_elementos; i++)
 	{
 		if(!adyaccencia_a_b(elementos.at(i), elementos.at(i))) return false;
 	}
 	return true;
 }
-void ingresar_tuplas_relacion()
+bool transitividad()
+{
+	int size_relaciones = relaciones.size();
+	for(int i = 0; i < size_relaciones; i++)
+	{
+		if(!adyaccencia_a_b(relaciones.at(i).second, relaciones.at(i).first)) return false;
+	}
+	return true;
+}
+void ingresar_elementos_relaciones()
 {
 	char tmp_c[256], tmp_c2[256];
 	string tmp, tmp2;
-	scanf("%d", &n_elementos);
+	fscanf(pFile, "%d",&n_elementos);
+	for(int i =0; i<n_elementos;i++)
+	{
+	fscanf(pFile,"%s", tmp_c);
+	tmp = tmp_c;
+	elementos.push_back(tmp_c);
+    }
+	fscanf(pFile,"%d", &n_elementos);
 	for(int i =0; i < n_elementos;i++)
 	{
-		scanf("%s %s", tmp_c, tmp_c2);
+		fscanf(pFile,"%s %s", tmp_c, tmp_c2);
 		tmp = tmp_c; tmp2 = tmp_c2;
 		adicionar_tupla(tmp, tmp2);
 		relaciones.push_back(make_pair(tmp, tmp2));
@@ -88,8 +96,11 @@ void print_elementos()
 	printf("%s\n", elementos.at(i).c_str());
 }
 int main (int args, char** argv){
-	ingresar_elementos();
-	ingresar_tuplas_relacion();
-	printf("%d", reflexividad());
+	pFile = fopen ("elementos_relaciones.txt","r");
+    rewind (pFile);
+	ingresar_elementos_relaciones();
+	printf("reflexividad: %d \n", reflexividad());
+	printf("transitividad %d \n", transitividad());
+	fclose (pFile);
 	return 0;
 }
