8f389738549d3bd116e1a0685adb336c360056d8
luchoman08 <luchoman08@gmail.com>
2016-04-08 15:26:50 -0500

    intento de transitividad si nerrorres

--- reflexividad/reflexivilidad.cpp
+++ reflexividad/reflexivilidad.cpp
@@ -4,13 +4,11 @@
 #include <utility>
 #include <string>
 #include <map>
-
+#include <algorithm>    // std::find
 /*  para garantizar 60 frames por segundo se necesita recalcular cada ecena en menos de 16 ms  */
 using namespace std;
 
 typedef map<string, vector<string> > adyacencia; //<elemento, vertices>
-typedef pair<string, map<string,int>*> im;
-typedef pair<string, int> si;
 FILE * pFile;
 adyacencia  ady;
 vector<pair<string, string> > relaciones;
@@ -27,26 +25,28 @@ int adicionar_tupla(string a, string b)
 	if(p == ady.end())
 	{
 		vector<string> s;
+		
 		s.push_back(b);
-		ady.insert(im(a, s));
+		ady.insert(make_pair(a, s));
 		return 0;
 	}
 	else
 	{
-		p->second.pushback(b);
+		p->second.push_back(b);
 		return 1;
 	}
 }
 bool adyaccencia_a_b(string a, string b)
 {
-  map<string, map<string,int>*>::iterator p = ady.find(a);
+  adyacencia::iterator p = ady.find(a);
   if(p==ady.end())
   return false;
   else
   {
 	  
-	 map<string,int>::iterator q = p->second->find(b);
-	 if(q==p->second->end())
+	 std::vector<string>::iterator q;
+     q = find(p->second.begin(), p->second.end(), b);
+	 if(q==p->second.end())
 	 return false;
 	 else 
 	 return true;
@@ -86,45 +86,39 @@ bool reflexividad()
 3- verificar si (a,c) para cada c allado en 2 se cumple (a,c) 
 
 */
-//bool transitividad()
-//{
- //   map<string,int>* b;
- //   map<string,int>::iterator q ;
-//	vector< map<string,int>*> c;
-//	int relaciones_size = relaciones.size();
-//	
-//	for(int i=0; i<relaciones_size; i++){
-//	   	map<string, map<string,int>*>::iterator p = ady.find(relaciones.at(i).first);
- //       b=p->second;
- //        
-  //     	std::map<string,int>::const_iterator
- //   mit=b->begin(),
- //   mend=b->end();
-    
- // for(;mit!=mend;++mit) 
- //            {
-	//			 printf("b tiene %s", mit->first.c_str());
-	//			 c.push_back(ady.find(mit->first)->second);
-	//		 }
-			 
-   //       for (int j = 0; j<c.size(); j++)
-    //         {
-//				 std::map<string,int>::const_iterator
-   // mit2 (c.at(j)->begin()),
-  //  mend2(c.at(j)->end());
- // for(;mit2!=mend2;++mit2) 
-				
-     //        {
-	//			if(!adyaccencia_a_b(relaciones.at(i).first, q->first)) return false;
+bool transitividad()
+{
+    vector<string> b;
+    map<string,vector<string> >::iterator q ;
+	vector<string> c;
+	 c.erase(c.begin(), c.end());
+	int relaciones_size = relaciones.size();
+	
+	for(int i=0; i<relaciones_size; i++){
+	   	map<string, vector<string> >::iterator p = ady.find(relaciones.at(i).first);
+       b=p->second;
+        
+     //  printf("tamaño de b para b= %s: %d\n",relaciones.at(i).first.c_str(),b.size()); 
+          for(int j=0; j<b.size(); j++)
+          {
+			  adyacencia::iterator r = ady.find(b.at(j));
+			  if(r!=ady.end()){
+			  //printf("el r tiene como tamaño: %d\n", r->second.size());
+			  c.insert(c.end(),r->second.begin(),r->second.end());
+		  }
+		  } 
+	
+       for(int j=0; j<c.size(); j++) {
+			if(!adyaccencia_a_b(relaciones.at(i).first, c.at(j))) return false;
+				//  printf("valores para %s: %s\n", relaciones.at(i).first.c_str(), c.at(j).c_str());
+			 }
 				  
-	//		 }
-	//			  
-	//		 }
-     //         c.clear();
+		 
+             c.erase(c.begin(), c.end());
          
-//	}
-//	return true;
-//}
+	}
+return true;
+}
 void ingresar_elementos_relaciones()
 {
 	char tmp_c[256], tmp_c2[256];
@@ -158,7 +152,7 @@ int main (int args, char** argv){
 	printf("reflexividad: %d \n", reflexividad());
 //	printf("simetria: %d \n", simetria());
 //	printf("antisimetria: %d \n", antisimetria());
-//	printf("transitividad: %d \n", transitividad());
+	printf("transitividad: %d \n", transitividad());
 	fclose (pFile);
 	return 0;
 }
