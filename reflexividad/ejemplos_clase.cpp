4650433baadb75484b7bc40c22bf56398797e210
luchoman08 <luchoman08@gmail.com>
2016-04-08 15:59:02 -0500

    intento de transitividad si nerrorres

---
+++ reflexividad/ejemplos_clase.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+void imprimir_x_y_matriz(int x, int y, int* mat, int nfil, int ncol)
+{
+	printf("Imprimir posicion [%d][%d]: \n", x,y);
+	mat = (mat + y * (nfil-1));
+	mat += (x-1);
+	printf("%d", *mat);
+}
+void imprimir_matris_puntero(int* mat, int nfil, int ncol)
+{
+	printf("Imprimir matriz: \n");
+	for(int i=0; i<nfil*ncol;i++)
+	{
+		if(i%nfil==0&&i!=0)printf("\n");
+		printf("%d ", *mat+i);
+	}
+	printf("\n");
+}
+int main (int args, char** argv)
+{
+    
+	int mat[3][3] ={ {1,2,3},{4,5,6},{7,8,9}};
+	int* matapunter = &mat[0][0];
+	imprimir_matris_puntero(matapunter,3,3);
+	imprimir_x_y_matriz(3,3, matapunter, 3 ,3 );
+	return 0;
+	
+}
